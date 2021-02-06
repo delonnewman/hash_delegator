270fe140fa9eddd7b4e9a65f39c515679e0ac265
Delon Newman <delnewman@salud.unm.edu>
2021-02-05 18:37:02 -0700

    Added transform_keys, required_attributes follows inheritance

--- lib/hash_delegator.rb
+++ lib/hash_delegator.rb
@@ -1,27 +1,61 @@
-require "hash_delegator/version"
+#require "hash_delegator/version"
 
+# Provides delegation and basic validation for Hashes
 class HashDelegator
   class << self
-    def require(*attributes)
-      @required_attributes = attributes
-    end
-
+    @@required_attributes = nil
     def required_attributes
-      @required_attributes
+      @@required_attributes
     end
 
-    def present_all(records)
-      records.map(&method(:new))
+    def require(*attributes)
+      if @@required_attributes.nil?
+        @@required_attributes = attributes
+      else
+        @@required_attributes += attributes
+      end
+
+      self
+    end
+
+    def transform_keys(&block)
+      @@key_transformer = block
+    end
+
+    @@key_transformer = nil
+    def key_transformer
+      @@key_transformer
+    end
+
+    def [](option)
     end
   end
 
-  extend Forwardable
-  delegate [] => :to_h
+  MUTATING_METHODS = [
+    :[]=,
+    :clear,
+    :delete,
+    :update,
+    :delete_if,
+    :keep_if,
+    :compact!,
+    :filter!,
+    :merge!,
+    :reject!,
+    :select!,
+    :transform_keys!,
+    :transform_values!,
+    :default=,
+    :default_proc=,
+    :compare_by_identity,
+    :rehash
+  ].reduce(Hash.new(false)) { |h, method| h.merge!(method => true) }.freeze
 
   def initialize(hash)
     raise "HashDelegator should not be initialized" if self.class == HashDelegator
 
     @hash = hash
+    @hash = @hash.transform_keys(&self.class.key_transformer) if self.class.key_transformer
 
     if self.class.required_attributes
       self.class.required_attributes.each do |attribute|
@@ -34,21 +68,33 @@ class HashDelegator
     @hash
   end
 
-  def [](key)
-    @hash[key.to_s]
+  def to_s
+    "#<#{self.class} #{@hash.inspect}>"
   end
+  alias inspect to_s
 
-  def key?(key)
-    @hash.key?(key.to_s)
+  def [](key)
+    if self.class.key_transformer
+      @hash[self.class.key_transformer.call(key)]
+    else
+      @hash[key]
+    end
   end
 
   def respond_to?(method)
-    super(method) || key?(method)
+    super(method) || key?(method) || hash_respond_to?(method)
   end
 
-  def method_missing(method)
-    return self[method] if key?(method)
+  def method_missing(method, *args, &block)
+    return @hash[method] if @hash.key?(method)
+    return @hash.public_send(method, *args, &block) if hash_respond_to?(method)
 
     raise NoMethodError, "undefined method `#{method}' for #{self}:#{self.class}"
+  end
+
+  private
+
+  def hash_respond_to?(method)
+    !MUTATING_METHODS[method] && @hash.respond_to?(method)
   end
 end
